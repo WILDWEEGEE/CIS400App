e376ec8af2f0b4ab4089082decf8d52dee7f38fd
Gianluca Gross <ggross@seas.upenn.edu>
2018-12-01 23:47:14 -0500

    Private repo authentication proof of concept

--- PetabloxApp/index.js
+++ PetabloxApp/index.js
@@ -1,5 +1,14 @@
 const shell = require('shelljs');
+const jsonwebtoken = require('jsonwebtoken');
 const home_dir = shell.pwd().stdout;
+const appId = 20461;
+
+const fs = require('fs');
+let privateKey = 'temp';
+fs.readFile('../../PrivateKeys/petablox.2018-12-01.private-key.pem', 'utf8', function(err, data) {  
+    if (err) throw err;
+    privateKey = data;
+}); 
 
 /**
  * This is the entry point for your Probot App.
@@ -128,10 +137,38 @@ module.exports = app => {
     app.on('issues.opened', async context => {
         console.log("hi");
 
-        
+        const now = Math.floor(Date.now() / 1000);
+        const payload = {
+            iat: now,
+            exp: now + 60,
+            iss: appId
+        };
+        const bearer = jsonwebtoken.sign(payload, privateKey, {algorithm: 'RS256'});
+        console.log(bearer);
+
+        context.github.authenticate({ type: 'app', token: bearer });
 
         const install_id = context.payload.installation.id;
-        console.log(context.github.apps.createInstallationToken({installation_id: install_id}));
+        const {data: {token}} = await context.github.apps.createInstallationToken({installation_id: install_id});
+
+        context.github.authenticate({ type: 'token', token });
+
+        console.log(token);
+
+        const fullName = context.payload.repository.full_name;
+        shell.exec(`git clone https://x-access-token:${token}@github.com/${fullName}.git`);
+
+        // const rubyCmd = shell.exec('ruby ruby/generate_jwt.rb', {silent:true});
+        // if (rubyCmd.code) {
+        //     error = 'Error: Could not generate JWT!';
+        //     console.log(error);
+        // } else {
+        //     const jwt = rubyCmd.stdout;
+        //     console.log(jwt);
+        // }
+
+        // const install_id = context.payload.installation.id;
+        // console.log(context.github.apps.createInstallationToken({installation_id: install_id}));
 
         // console.log(context.payload.installation.id);
 
@@ -139,9 +176,9 @@ module.exports = app => {
         // console.log(context.github.apps.listInstallations());
         // console.log(octokit.apps.getAuthenticated());
 
-        console.log(home_dir);
-        const issueComment = context.issue({ body: 'Thanks for opening this issue!' });
-        return context.github.issues.createComment(issueComment);
+        // console.log(home_dir);
+        // const issueComment = context.issue({ body: 'Thanks for opening this issue!' });
+        // return context.github.issues.createComment(issueComment);
     });
 
     // app.on('pull_request.opened', async context => {
